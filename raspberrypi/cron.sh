eb7cc3d75cd524ded51dde5b476beca53b42099a
Abhinav <abhixec@gmail.com>
2016-03-04 23:18:09 -0800

    changing the equals

--- raspberrypi/cron.sh
+++ raspberrypi/cron.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#set -ex
+set -ex
 MESSAGE=$(~/whatismyip.py) 
 if ! [ -f ~/.my.ip ]; then
   echo $MESSAGE > ~/.my.ip
 else
   PREVIOUS_IP=$(cat ~/.my.ip) 
-  if [ "$PREVIOUS_IP" == "$MESSAGE" ]; then
+  if [ "$PREVIOUS_IP" = "$MESSAGE" ]; then
 	:
   else
     /usr/local/bin/ntfy send  $MESSAGE
