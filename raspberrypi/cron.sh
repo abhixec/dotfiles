b9bb91afaec2de3a94a40b278eec6539766fdddc
Abhinav <abhixec@gmail.com>
2016-03-04 20:08:39 -0800

    addding some intelligence to sending the ip to my android phone

--- raspberrypi/cron.sh
+++ raspberrypi/cron.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-
+#set -ex
 MESSAGE=$(~/whatismyip.py) 
-/usr/local/bin/ntfy send  $MESSAGE
+if ! [ -f ~/.my.ip ]; then
+  echo $MESSAGE > ~/.my.ip
+else
+  PREVIOUS_IP=$(cat ~/.my.ip) 
+  if [ "$PREVIOUS_IP" == "$MESSAGE" ]; then
+	:
+  else
+    /usr/local/bin/ntfy send  $MESSAGE
+  fi
+fi
