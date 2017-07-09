6de7b64bff987fd9ef4c9bb12e4f8a2503245ee3
abhixec <abhixec@gmail.com>
2017-07-08 20:25:17 -0700

    Add zsh files

---
+++ zsh/aliases.zsh
@@ -0,0 +1,10 @@
+# Aliases for zsh 
+# Abhinav <abhixec@abhixec.com>
+
+alias zmv="zmv -wM"
+alias zcp="zmv -wC"
+alias zln="zmv -wL"
+
+# Open the zshrc
+alias zrc="vim ~/.zshrc"
+alias zre="source ~/.zshrc"
---
+++ zsh/completion.zsh
@@ -0,0 +1,7 @@
+
+autoload -Uz compinit && compinit
+zstyle ':completion:*' list-colors "${LS_COLORS}" # Complete with same colors as ls.
+zstyle ':completion:*' max-errors 2 # Be lenient to 2 errors.
+zstyle ':completion:*' completer _expand _complete _correct _approximate # Completion modifiers.
+zstyle ':completion:*' use-cache true # Use a completion cache.
+zstyle ':completion:*' ignore-parents pwd # Ignore the current directory in completions.
