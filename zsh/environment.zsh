5e2bcdae6d77f0e4a0cf4b5293b3e13747a716c8
abhixec <abhixec@gmail.com>
2017-08-08 18:01:11 -0700

    Move env. stuff to environment.zsh

---
+++ zsh/environment.zsh
@@ -0,0 +1,16 @@
+# Sets up the working Shell 
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=100000
+SAVEHIST=100000
+setopt appendhistory autocd extendedglob nomatch notify
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/Users/abhinav/.zshrc'
+
+# Export PATH
+export PATH=~/.local/bin/:$PATH
+
+# Export PATH for rust
+export PATH=$HOME/.cargo/bin:$PATH
