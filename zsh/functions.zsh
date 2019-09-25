5a9f6f26163106a658db5fc4e29438f56ac5ccd1
Abhinav <abhixec@gmail.com>
2019-09-24 17:30:55 -0700

    Function to set blog directory

---
+++ zsh/functions.zsh
@@ -0,0 +1,24 @@
+#Functions
+
+setBlog(){
+    echo "Enter the directory for the blog"
+    read blogDir
+    if [ -f ~/.configs ]; then
+	echo "export blogDir=$blogDir" > ~/.configs
+    else
+	echo "creating configs file in $HOME"
+	echo "export blogDir=$blogDir" > ~/.configs
+    fi
+    
+}
+
+# set blog
+blog(){
+if [ -z ${blogDir} ]; then
+    echo "Blog Dir not set";
+    setBlog;
+else
+    cd $blogDir
+fi
+}
+
